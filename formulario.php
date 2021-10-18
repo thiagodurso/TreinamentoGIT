435d5ea3c2bc0bfafd19e1c292ca48aae6c98dd5
Thiago Durso <73541189+thiagodurso@users.noreply.github.com>
2021-10-18 17:58:39 -0300

    Adicionei o formulario.php

---
+++ formulario.php
@@ -0,0 +1,19 @@
+<?php 
+
+				
+				$num1 = $_POST["n1"];
+				$num2 = $_POST["n2"];
+				$n1=$num1;
+				$n2 = $num2;
+
+				$soma=0;
+				$soma = $n1 + $n2;
+				$divisao =  $n1/$n2;
+				$multiplicacao = $n1*$n2;
+				$subtracao =  $n1-$n2;
+
+				echo "Soma: " .$soma ."<br> Divisão: ".$divisao . "<br> multiplicacao: " .$multiplicacao
+				."<br> Subtração: " .$subtracao;
+
+
+ ?>
