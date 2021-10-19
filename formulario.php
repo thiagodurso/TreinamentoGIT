77114cabe81605fba36b50f986c56b8ea39e4922
Guilherme Rosa <57267848+guilhermrosa@users.noreply.github.com>
2021-10-19 20:23:27 -0300

    Correção.

--- formulario.php
+++ formulario.php
@@ -1,18 +1,14 @@
 <?php 
 
-				
-				$num1 = $_POST["n1"];
-				$num2 = $_POST["n2"];
-				$n1=$num1;
-				$n2 = $num2;
+				$n1= $_POST["n1"];
+				$n2 =  $_POST["n2"];
 
-				$soma=0;
 				$soma = (int)$n1 + (int)$n2;
 				$divisao =  $n1/$n2;
 				$multiplicacao = $n1*$n2;
 				$subtracao =  $n1-$n2;
 
-				echo "Soma: " .$soma ."<br> Divisão: ".$divisao . "<br> multiplicacao: " .$multiplicacao
+				echo "Soma: " .$soma ."<br> Divisão: ".$divisao . "<br> Multiplicacao: " .$multiplicacao
 				."<br> Subtração: " .$subtracao;
 
 
